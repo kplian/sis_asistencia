4d1f0029f731825447841f02d0477aa6cf9a0e11
Rodrigo Chumacero <rodrychm@gmail.com>
2019-04-16 09:52:54 -0400

    Datos base

--- base/data000001.sql
+++ base/data000001.sql
@@ -7,5 +7,12 @@ VALUES
   (E'ASIS', E'Sistema de Asistencia', E'2019-01-30', E'ASIS', E'activo', E'asistencia', NULL);
 
 /***********************************F-DAT-RAC-ASIS-48-30/01/2019*****************************************/
+/***********************************I-DAT-MMV-ASIS-48-16/04/2019*****************************************/
+select pxp.f_insert_tgui ('SISTEMA DE ASISTENCIA', '', 'ASIS', 'si', 1, '', 1, '', '', 'ASIS');
+select pxp.f_insert_tgui ('Mes Trabajo', 'Mes Trabajo', 'MTO', 'si', 1, 'sis_asistencia/vista/mes_trabajo/MesTrabajoReg.php', 2, '', 'MesTrabajoReg', 'ASIS');
+select pxp.f_insert_tgui ('Mes Trabajo VoBo', 'Mes Trabajo VoBo', 'VOM', 'si', 2, 'sis_asistencia/vista/mes_trabajo/MesTrabajoVoBo.php', 2, '', 'MesTrabajoVoBo', 'ASIS');
+select pxp.f_insert_tgui ('Tipo Aplicación ', 'Tipo Aplicación', 'TPS', 'si', 3, 'sis_asistencia/vista/tipo_aplicacion/TipoAplicacion.php', 2, '', 'TipoAplicacion', 'ASIS');
+/***********************************F-DAT-MMV-ASIS-48-16/04/2019*****************************************/
+
 
  
--- base/dependencies000001.sql
+++ base/dependencies000001.sql
@@ -4,3 +4,52 @@ select pxp.f_insert_testructura_gui ('MTO', 'ASIS');
 select pxp.f_insert_testructura_gui ('VOM', 'ASIS');
 select pxp.f_insert_testructura_gui ('TPS', 'ASIS');
 /***********************************F-DEP-RAC-ASIS-48-15/04/2019*****************************************/
+/***********************************I-DEP-MMV-ASIS-48-16/04/2019*****************************************/
+CREATE VIEW asis.vtotales_horas (
+    id_funcionario,
+    id_periodo,
+    id_planilla,
+    estado,
+    total_normal,
+    total_extra,
+    total_nocturna,
+    extra_autorizada)
+AS
+SELECT ta.id_funcionario,
+    ta.id_periodo,
+    ta.id_planilla,
+    ta.estado,
+    sum(de.total_normal) AS total_normal,
+    sum(de.total_extra) AS total_extra,
+    sum(de.total_nocturna) AS total_nocturna,
+    sum(de.extra_autorizada) AS extra_autorizada
+FROM asis.tmes_trabajo_det de
+     JOIN asis.tmes_trabajo ta ON ta.id_mes_trabajo = de.id_mes_trabajo
+GROUP BY ta.id_funcionario, ta.id_periodo, ta.id_planilla, ta.estado;
+
+CREATE VIEW asis.vtotales_horas_centro_costo (
+    id_funcionario,
+    id_periodo,
+    id_planilla,
+    id_centro_costo,
+    estado,
+    total_normal,
+    total_extra,
+    total_nocturna,
+    extra_autorizada)
+AS
+SELECT ta.id_funcionario,
+    ta.id_periodo,
+    ta.id_planilla,
+    de.id_centro_costo,
+    ta.estado,
+    sum(de.total_normal) AS total_normal,
+    sum(de.total_extra) AS total_extra,
+    sum(de.total_nocturna) AS total_nocturna,
+    sum(de.extra_autorizada) AS extra_autorizada
+FROM asis.tmes_trabajo_det de
+     JOIN asis.tmes_trabajo ta ON ta.id_mes_trabajo = de.id_mes_trabajo
+GROUP BY ta.id_funcionario, ta.id_periodo, ta.id_planilla, de.id_centro_costo,
+    ta.estado;
+/***********************************F-DEP-MMV-ASIS-48-16/04/2019*****************************************/
+
