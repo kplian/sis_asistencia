1edbb3a55def02e617d2064cc57449fc9f2c07b0
Rodrigo Chumacero <rodrychm@gmail.com>
2019-11-05 11:03:30 -0400

    #22 nombre de archivo

--- control/ACTMesTrabajoDet.php
+++ control/ACTMesTrabajoDet.php
@@ -215,10 +215,10 @@ class ACTMesTrabajoDet extends ACTbase{
     }
     function subirArchivoExcelCc(){ //#18
         //validar extnsion del archivo
-        if($this->objParam->getParametro('periodo') <= 9){
+        if($this->objParam->getParametro('periodo') < 10){ //#19
             $mes = '0'.$this->objParam->getParametro('periodo');
         }else{
-            $mes = '0'.$this->objParam->getParametro('periodo');
+            $mes = $this->objParam->getParametro('periodo'); //19
         }
         $codigoPeriodoGestion = $this->objParam->getParametro('desc_codigo').'_'.$mes.$this->objParam->getParametro('gestion');
 
